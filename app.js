07663fc5f0ca8080a90b919b8d1040b3b5240fe5
Farid Wilhelm Zimmermann <farwilhelm@gmail.com>
2018-02-20 13:20:39 +0000

    Improved error handling

--- app.js
+++ app.js
@@ -12,28 +12,46 @@ app.get("/", (req, res) => {
     res.render('index');
 })
 
-app.get("/new/*", (req, res) => {
+app.get("/new/*", (req, res, next) => {
     
     if( db.verifyUrl(req.params[0])) {
         db.create(req.params[0], (err, result) => {
-            if (err) return res.send(err);
+            if (err) {
+                let err = new Error('Database Error');
+                err.status = 500;
+                next(err);
+            }
             res.send(`Access ${result.baseUrl} at https://slice-that-url.herokuapps.com/r/${result.shortUrl}`)
         }); 
     } else {
-        res.send(`${req.params[0]} is not a valid Url! Please try again`);
+        let err = new Error(`${req.params[0]} is not a valid Url! Please try again`);
+        err.status = 400;
+        next(err);
+        
     }
 });
 
-app.get("/r/:url", (req, res) => {
+app.get("/r/:url", (req, res, next) => {
     db.redirect(req.params.url, (err, result) => {
-        if (err) return res.send(err);
+        if (err) {
+            let err = new Error('Database Error');
+            err.status = 500;
+            next(err);
+        }
         if (result) {
             res.redirect(result.baseUrl);
         } else {
-            res.send(`Url not found. Are you sure ${req.params.url} is the right identifier?`)
+            let err = new Error(`Url not found. Are you sure ${req.params.url} is the right identifier?`)
+            err.status = 412;
+            next(err);
         }
         
     })
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.send(`Error: ${err.status}, ${err.message}`)
 })
 
 app.listen(process.env.PORT || 3000, () => {
